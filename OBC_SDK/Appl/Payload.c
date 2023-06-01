b2d919e9c9e7a32f03ea0231594404084ec570b3
Vanessa Whitehead <70388031+vanessaWhitehead@users.noreply.github.com>
2023-06-01 14:52:17 -0500

    Delete files

--- OBC_SDK/Appl/Payload.c
+++ OBC_SDK/Appl/Payload.c
@@ -2,16 +2,22 @@
  * Payload.c
  *
  *  Created on: Oct 2, 2021
- *  Updated on: Nov 1, 2021
+ *  Modified on: Nov 1, 2021
+ *  Modified on Apr 23, 2023
  *      Author: Alexis Aurandt
+ *      		Manas Mathur
  */
 
-#include <Payload.h>
-#include <CySatPacketProtocol.h>
-#include <helper_functions.h>
+#include <stdio.h>
 #include <string.h>
 #include <fatfs.h>
 #include <math.h>
+#include <Payload.h>
+#include <CySatPacketProtocol.h>
+#include <helper_functions.h>
+
+
+
 
 /*************************** CALIBRATION FUNCTIONS *****************************/
 /**
@@ -169,9 +175,9 @@ HAL_StatusTypeDef RESET_PAYLOAD_TO_DEFAULTS(bool* default_status){
  * @param spavg: The spavg value
  */
 HAL_StatusTypeDef GET_PAYLOAD_VALUES(float* bandwidth, float* calib_1,
-                                    float* calib_2, float* gain, float* fftsize,
-                                    float* frequency, float* integral,
-                                    float* rate, float* spavg){
+                                     float* calib_2, float* gain, float* fftsize,
+                                     float* frequency, float* integral,
+                                     float* rate, float* spavg){
     uint8_t data[24];
     HAL_StatusTypeDef status = PAYLOAD_READ(0x17, data, 24);
     if(status != HAL_OK){
@@ -228,11 +234,14 @@ HAL_StatusTypeDef TAKE_MEASUREMENT(uint16_t time){
     return status;
 }
 
+
 /**
  * @brief Private function to transfer file type
  *
  * @param file_type: 0 = dat file and 1 = kelvin file
  */
+
+/**
 HAL_StatusTypeDef FILE_TRANSFER(int file_type){
     // Start transfer request to payload
     CySat_Packet_t packet;
@@ -287,43 +296,123 @@ HAL_StatusTypeDef FILE_TRANSFER(int file_type){
         byte_sum += data[i];
     }
 
-
-
     if(packet.Data[0] == (0xFF - (byte_sum & 0xFF))){
         //Transfer data to computer for debugging
         for(int i = 0; i < file_size; i++){
             debug_printf("%d", data[i]);
         }
-        //Write to SD Card
-        FATFS FatFs; //Fatfs handle
-        FIL fil; //File handle
-        FRESULT fres; //Result after operations
 
-        //Open the file system
-        fres = f_mount(&FatFs, "0", 1);
-        if(fres != FR_OK){
-            return HAL_ERROR;
+        //Write to SD Card
+        //NOTE: Code to read from entry number file inserted after successful testing in AppTasks, may need some tweaks to run in Payload
+
+        FATFS FatFs; //Fatfs handle
+        //FIL fil; //File handle
+        FIL entryfil; //File containing data entry number
+        FRESULT fres; //Result after operations
+        FRESULT efres; //Result after opening entryfil
+        FRESULT success;
+
+        FRESULT res; //FatFs function common result code
+        UINT byteswritten, bytesread; // File write/read counts
+        TCHAR const* SDPath = "0";
+        uint8_t rtext[_MAX_SS];//File read buffer
+
+        if(f_mount(&FatFs, "", 0) != FR_OK)
+        {
+        debug_printf("[SD Write/ERROR]: Failed to mount SD drive");
+        //Error_Handler();
         }
-        if(file_type == 0)
-            fres = f_open(&fil, "test_dat.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
         else
-            fres = f_open(&fil, "test_kelvin.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
-        if(fres != FR_OK){
-            return HAL_ERROR;
+        {
+        	debug_printf("[SD Write/SUCCESS]: SD drive mounted successfully");
+            //Open file for writing (Create)
+        	fres = f_open(&entryfil, "entry.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE);
+            if(fres != FR_OK)
+            {
+            	debug_printf("[SD Write/ERROR]: Failed to open entry number file");
+            }
+            else
+            {
+    			debug_printf("[SD Write/SUCCESS]: Entry number file opened successfully");
+
+            	char temp_bytes [8]="00000000";
+
+            	long int entry_id = 0;
+    			success = f_read(&entryfil, &temp_bytes, 8, &bytesread);
+    			sscanf(&temp_bytes,"%ld",&entry_id);
+
+    			//If no data entry value is present, provides a starting value
+    			if(success != FR_OK)
+    			{
+    				entry_id = 0;
+    				debug_printf("[SD Write]: Entry number file created");
+    			}
+
+    			debug_printf("[SD Write]: Old entry id char and long: %c%c%c%c%c%c%c%c %ld", temp_bytes[0],temp_bytes[1],temp_bytes[2],temp_bytes[3],temp_bytes[4],temp_bytes[5],temp_bytes[6],temp_bytes[7],entry_id);
+
+    			//Adds 1 to the data entry number
+    			long int new_entry_id = entry_id + 1;
+
+    			debug_printf("[SD Write]: New entry id: %ld", new_entry_id);
+
+    			char new_entry_str[8]="00000000";
+    			sprintf(new_entry_str, "%ld", new_entry_id);
+
+    			debug_printf(new_entry_str);
+
+    			//Write to the text file, rewinding first
+    			res = f_lseek(&entryfil, 0);
+    			res = f_write(&entryfil, new_entry_str, strlen((char *)new_entry_str), (void *)&byteswritten);
+
+    			if(res != FR_OK)
+    			{
+    				debug_printf("[SD Write]: Write Unsuccessful");
+    			}
+
+    			//Closes the file
+    			if((byteswritten == 0) || (res != FR_OK))
+    			{
+    				debug_printf("[SD Write/ERROR]: Failed write to entry number file");
+    			}
+    			else
+    			{
+    				f_close(&entryfil);
+    			}
+
+			//Create the specified data file
+
+			char data_file_name[12]={"\0"};
+
+			if(file_type == 0){
+				data_file_name[0] = sprintf(filename_buff, "dat%d.txt", entry_id);
+				debug_printf("dat file");
+			}
+			else {
+				data_file_name[0] = sprintf(filename_buff, "kel%d.txt", entry_id);
+				debug_printf("kel file");
+			}
+
+			debug_printf("%s", data_file_name);
+
+			fres = f_open(&fil, data_file_name, FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+
+			if(fres != FR_OK){
+				return HAL_ERROR;
+			}
+			UINT bytes;
+			fres = f_write(&fil, (char*)data, (UINT)file_size, &bytes);
+			if(fres != FR_OK || bytes!= file_size){
+				//status = HAL_ERROR;
+				return HAL_ERROR;
+			}
+			f_close(&fil); //Close the file
+			f_mount(NULL, "", 0); //De-mount the drive
         }
-        UINT bytes;
-        fres = f_write(&fil, (char*)data, (UINT)file_size, &bytes);
-        if(fres != FR_OK || bytes!= file_size){
-            //status = HAL_ERROR;
-        }
-        f_close(&fil); //Close the file
-        f_mount(NULL, "", 0); //De-mount the drive
     }
-    else{
-        return HAL_ERROR;
-    }
+
     return status;
 }
+*/
 
 /**
  * @brief Commands the payload to transfer the DAT file
@@ -342,28 +431,256 @@ HAL_StatusTypeDef KELVIN_FILE_TRANSFER(){
 }
 
 /**
- * @brief Selects which part of the data is transmitted and sends that part home 
- * 
-*/
-HAL_StatusTypeDef CODE_SEPERATOR(int measurementID, int dataType, int startPacket, int endPacket){
-    // Read OBC's SD card (and possibly erases it from the payload)
+ * @brief Deletes the specified data file from the SD card
+ *  Deletes the DAT file
+ */
+HAL_StatusTypeDef DELETE_DATA_FILE_DAT(int data_file_no){
 
-    
-    char * dataTypeStr = dataType == 0 ? ".kelvin" : ".dat"; // 0 = kelvin, 1 = dat
-    char *fileName = asprintf("%d%s", measurementID, dataTypeStr); 
-    
-    FILE *fp = fopen(fileName, "r");
+    //UNFINISHED, LIKELY NONFUNCTIONAL: Needs testing and error checking!
+    //Needs to be linked to a command (packet)
 
-    fseek(fp, 0, SEEK_END); // seek to end of file
-    long int endFileLength = ftell(fp); // get current file pointer
-    fseek(fp, 0, SEEK_SET); // seek back to beginning of file
-    // proceed with allocating memory and reading the file
+    FATFS FatFs;
+    FRESULT file_del;
 
-    
-    // increment 
+    if(f_mount(&FatFs, "", 0) != FR_OK)
+    {
+        debug_printf("File deletion unsuccessful: Failed to mount SD drive.");
+        return HAL_ERROR;
+    }
+    else {
+
+        //int num_length = (data_file_no==0)?1:log10(data_file_no)+1;
+        //char file_name[num_length + 8]; // 8 = num of characters in dat.txt + 1
+
+        sprintf(file_name, "dat%d.txt", data_file_no);
+
+        filedel = f_unlink(file_name);
+
+        if(filedel == FR_OK){
+            debug_printf("Data file %d deleted successfully.", data_file_no);
+            return HAL_OK; //Change this later to correspond with a packet-sending operation
+        }
+
+        else {
+            debug_printf("File deletion unsuccessful: File not found.");
+            return HAL_ERROR;
+        }
+    }
 }
 
+
+/**
+ * @brief Deletes the specified data file from the SD card
+ *  Deletes the KEL file
+ */
+HAL_StatusTypeDef DELETE_DATA_FILE_KEL(int data_file_no){
+    // It's going to be very similar to DELETE_DATA_FILE_DAT
+    // deletes kel%d.txt
+
+    // make the file name using the data_file_no
+    int num_length = (data_file_no==0)?1:log10(data_file_no)+1; // number of digits in file no
+    char file_name[num_length + 8]; // length of full name 8 = num of characters in kel.txt + 1
+    sprintf(file_name, "kel%d.txt", data_file_no);
+
+    // try to delete
+    if( remove(file_name) != 0 ) {
+        perror("Error deleting file");
+    }
+
+    else {
+        puts("File successfully deleted");
+    }
+}
+
+/**
+ * @brief Deletes the specified data file from the SD card
+ *  Deletes the KEL and DAT file
+ */
+HAL_StatusTypeDef DELETE_DATA_FILE(int data_file_no){
+
+    DELETE_DATA_FILE_KEL(data_file_no);
+    DELETE_DATA_FILE_DAT(data_file_no);
+
+}
+
+
+
+
+/**
+ * @brief Selects which part of the data is transmitted and sends that part home
+ *
+*/
+
+HAL_StatusTypeDef PACKET_PRINT(){
+    debug_printf("This is a test");
+}
+
+
+HAL_StatusTypeDef PACKET_SEPARATOR(unsigned short int measurementID, unsigned short int dataType, unsigned short int startPacket, unsigned short int endPacket){
+    debug_printf("very beginning");
+    FIL currfile; //File containing data entry number
+    debug_printf("after currfile");
+    FRESULT fres; //Result after operations
+
+
+    debug_printf("Past declarations");
+    if (startPacket > endPacket) //Checks to make sure packet ordering is valid
+    {
+        debug_printf("[PACKET_SEPARATOR/ERROR]: Start Packet is greater than End Packet");
+        return HAL_ERROR;
+    }
+    debug_printf("Past ordering check");
+    //char dataTypeStr [7]="";
+    char *dataTypeStr = dataType == 0 ? ".kelvin" : ".dat"; // 0 = kelvin, 1 = dat
+
+//    if(dataType==0){
+//    	char dataTypeStr[7] = ".kelvin";
+//    }else if(dataType==1){
+//    	char dataTypeStr[4] = ".dat";
+//    }else{
+//    	debug_printf("[PACKET_SEPARATOR/ERROR]: Invalid data type");
+//    	fres = f_unmount ("");
+//    	return HAL_ERROR;
+//    }
+
+
+    debug_printf("Past dataType");
+    //char fileName = sprintf("%d%s", measurementID, &dataTypeStr); // Grabs the file from the SD card
+    //char fileName="0.kelvin";
+    //debug_printf("File name: %s",fileName);
+
+    fres = f_open(&currfile, "entry18.txt", FA_OPEN_ALWAYS | FA_READ);
+    if(fres != FR_OK)
+    {
+        fres = f_unmount ("");
+        debug_printf("[PACKET_SEPARATOR/ERROR]: Failed to open measurement file");
+    }
+
+    //fseek(fp, 0L, SEEK_END);
+    long unsigned int sizeFile = f_size(&currfile);
+    debug_printf("File size is %lu",sizeFile);
+
+    //if (fseek(fp, 118*startPacket, 0) != 0) // seek to start of data
+    //{
+    //    fclose(fp);
+    //    return HAL_ERROR;
+    //}
+
+
+
+
+
+    for (unsigned short int i = startPacket; i <= endPacket; i++)
+    {
+        char packet[128];
+        // Header data
+        packet[0] = 0xFF;
+
+        char id[2]="00";
+        sprintf(id, "%02d", measurementID);
+
+        char id2[2]="00";
+        sprintf(id2, "%02d", i);
+
+        //debug_printf("id: %s id2: %s dataType: %d", id, id2,dataType);
+        //packet[1] = (measurementID >> 8) & 0xFF;
+        //packet[2] = measurementID & 0xFF;
+
+        packet[1]=id[0];
+        packet[2]=id[1];
+
+        if(dataType==0){
+            packet[3]=48;
+        }else if(dataType==1){
+            packet[3]=49;
+        }
+        //packet[3] = dataType;
+
+        packet[4]=id2[0];
+        packet[5]=id2[1];
+
+        //packet[4] = (i >> 8) & 0xFF;
+        //packet[5] = i & 0xFF;
+
+
+        //PSEUDOCODE FOR: Check to see if packet requested is greater than the length of a file (if so break out of the loop)
+        // ASK STEVEN WHAT HE MEANS BY "A FILE"
+
+        char data[118] = {'\0'};
+        UINT bytesRead=0;
+
+        //size_t read  = fread(&data, 1, sizeFile - 118 * i, fp);
+        fres = f_read(&currfile, &data, 118, &bytesRead);
+        if(fres != FR_OK)
+        {
+            f_close(&currfile);
+            fres = f_unmount ("");
+            debug_printf("[PACKET_SEPARATOR]: Error reading file");
+            return HAL_ERROR;
+        }
+
+        for (int j = 0; j < bytesRead; j++) // Copy the data into the packet
+        {
+            packet[6+j] = data[j];
+        }
+
+        //crc32(packet, (sizeFile - bytesRead * i) + 6, &packet[6 + (sizeFile - bytesRead * i)]);
+        debug_printf("Packet %d: %s", i, packet);
+
+        //packet[0]="H";
+        //packet[1]="E";
+        //packet[2]="L";
+        //packet[3]="P";
+        //HAL_UART_Transmit(&huart1, packet, 6+ (sizeFile - bytesRead * i), 200); // Do not use for standalone testing!!!!!!
+        HAL_UART_Transmit(&huart1, packet, 120, 50);
+        osDelay(2000);
+    }
+
+
+    return HAL_OK;
+}
+
+/*
+*/
 /*************************** HELPER FUNCTIONS **********************************/
+
+
+
+//int packet_size(char *fileName, int packetNumber)
+//{
+//     #define SEGMENT 120 // approximate target size of small file long
+//
+//
+//    int segments=0, i, len, offset;
+//    FILE *fp1, *fp2;
+//
+//
+//    long sizeFile = file_size(fileName);
+//
+//    segments = sizeFile/SEGMENT + 1;  //ensure end of file
+//
+//    char smallFileName[260]; // small File Name
+//    char line[1080]; // not sure what this is doing
+//    fp1 = fopen(fileName, "r");
+//
+//
+//    if(fp1){
+//    	for(i=0;i<segments;i++){
+//    		offset = 0;
+//    		sprintf(smallFileName, "%s%d.txt", fileName, i);
+//    		fp2 = fopen(smallFileName, "w");
+//    		if(fp2){
+//    			while(fgets(line, 1080, fp1) && offset <= SEGMENT){
+//    				offset += strlen(line);//track size of growing file
+//    				fputs(line, fp2);
+//    			}
+//    			fclose(fp2);
+//    		}
+//    	}
+//    	fclose(fp1);
+//    }
+//}
+
 /**
  * @brief Sends a write command to the payload over UART
  * @param command: The command to be used on the transceiver
@@ -429,7 +746,7 @@ HAL_StatusTypeDef PAYLOAD_READ(uint8_t command, uint8_t* out_data_ptr, uint8_t o
     else if(packet.Data_Length != out_byte)
         return HAL_ERROR;
     else if(validateCySatChecksum(packet) != 1)
-            return HAL_ERROR;
+        return HAL_ERROR;
     return status;
 }
 
@@ -457,12 +774,12 @@ int convert_to_int(uint8_t* data_ptr, int length){
  * @param length : The length of the array that will be returned
  */
 void convert_to_bytes(uint8_t* data, int num, int length){
-    if (num < 0)
+    if (num < 0){
         num += pow(2, 8*length);
+    }
     int index = length - 1;
     for(int i = 0; i < length; i++){
         data[i] = (num >> (index*8)) & 0xFF;
         index--;
     }
 }
-
